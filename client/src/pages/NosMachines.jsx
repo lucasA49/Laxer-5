fa26154a27d2b234011c4e4557295415559ef53a
lucasA49 <lucasaksu@gmail.com>
2024-09-20 01:52:08 +0200

    mise en place du flavicon +  bouton de rediréction sur accueil sur le logo

---
+++ client/src/pages/NosMachines.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function NosMachines() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default NosMachines;
