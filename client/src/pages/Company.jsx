7c55ddd69946b3088aaf35c615f3a6a3077a83e8
jules <jules.aguesse@hotmail.com>
2024-09-19 17:20:42 +0200

    entreprise

---
+++ client/src/pages/Company.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Bodycompany from "../components/Bodycompany";
+
+function Company() {
+  return (
+    <>
+      <Header />
+      <Bodycompany />
+      <Footer />
+    </>
+  );
+}
+
+export default Company;
