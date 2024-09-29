93a0ed6dfaf840dbf7f20aa5f2c8e9370c01392d
AJxles <jules.aguesse@hotmail.com>
2024-09-29 22:08:16 +0200

    services

--- client/src/components/BodyServices.jsx
+++ client/src/components/BodyServices.jsx
@@ -1,10 +1,73 @@
 import "../style/BodyServices.css";
+import decoupealfa from "../assets/Image/decoupealfa.png";
+import Decoupe from "../assets/Image/Decoupe.jpg";
+import pieceplié from "../assets/Image/pieceplié.jpg";
 
 function BodyNosServices() {
   return (
-    <div>
-      <h1>Page de Nos Services</h1>
-      <p>Contenu de la page Body Nos Services</p>
+    <div className="services-contener">
+      <div className="service-item">
+        <div className="specifications">
+          <ul>
+            <li>1,500 mm de largeur de Tôle</li>
+            <li>3,000 mm de longueur de Tole</li>
+            <li>20 mm d'épaisseur de découpe sur de l'acier</li>
+            <li>12 mm d'épaisseur de découpe sur de l'inox</li>
+            <li>10 mm d'épaisseur de découpe sur de l'alu</li>
+          </ul>
+        </div>
+        <div className="service-text">
+          <h2>Découpe Laser 3D</h2>
+          <p>
+            Our 3D laser cutting workshop allows precise and customized cuts...
+          </p>
+        </div>
+        <div className="service-image">
+          <img src={decoupealfa} alt="Laser Cutting" />
+        </div>
+      </div>
+
+      <div className="service-item">
+        <div className="specifications">
+          <ul>
+            <li>1,500 mm de largeur de Tôle</li>
+            <li>3,000 mm de longueur de Tole</li>
+            <li>20 mm d'épaisseur de découpe sur de l'acier</li>
+            <li>12 mm d'épaisseur de découpe sur de l'inox</li>
+            <li>10 mm d'épaisseur de découpe sur de l'alu</li>
+          </ul>
+        </div>
+        <div className="service-text">
+          <h2>Découpe Laser</h2>
+          <p>
+            Our 3D laser cutting workshop allows precise and customized cuts...
+          </p>
+        </div>
+        <div className="service-image">
+          <img src={Decoupe} alt="Laser Cutting" />
+        </div>
+      </div>
+
+      <div className="service-item">
+        <div className="specifications">
+          <ul>
+            <li>1,500 mm de largeur de Tôle</li>
+            <li>3,000 mm de longueur de Tole</li>
+            <li>20 mm d'épaisseur de découpe sur de l'acier</li>
+            <li>12 mm d'épaisseur de découpe sur de l'inox</li>
+            <li>10 mm d'épaisseur de découpe sur de l'alu</li>
+          </ul>
+        </div>
+        <div className="service-text">
+          <h2>Pliage</h2>
+          <p>
+            Our 3D laser cutting workshop allows precise and customized cuts...
+          </p>
+        </div>
+        <div className="service-image">
+          <img src={pieceplié} alt="Laser Cutting" />
+        </div>
+      </div>
     </div>
   );
 }
